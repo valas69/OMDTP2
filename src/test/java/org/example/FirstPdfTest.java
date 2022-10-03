3763e55688a47b530c1a8bb97ae8621831bb67a1
Tergel Tsagaan <tergel.tsagaan@gmail.com>
2022-10-03 15:16:34 +0200

    add dependencies

---
+++ src/test/java/org/example/FirstPdfTest.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+public class FirstPdfTest extends TestCase {
+    public void testMain() throws Exception {
+      assertEquals(1, 1);
+    }
+}
